fcc215d8d6634614ebef2ec8cc546cceea8115ac
100Mbps <hanrongjie@hotmail.com>
2021-10-21 16:18:28 +0800

    添加项目文件。

---
+++ AsyncDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDemo
+{
+    class AsyncDemo
+    {
+        private int CounterHolder = 0;
+        private readonly HttpClient client = new HttpClient();
+        public async Task<int> CounterAsync() {
+            await Task.Delay(1000);
+            return CounterHolder++;
+        }
+        public async Task<string> GetWebContentAsync(string Url) {
+            return  await client.GetStringAsync(Url);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var i = 0;
+            AsyncDemo demo = new AsyncDemo();
+            while (i++ < 100) {
+                Console.WriteLine($"counter is ={demo.CounterAsync().Result}");
+                Console.WriteLine($"counter is ={demo.GetWebContentAsync("http://www.baidu.com").Result}");
+            }
+        }
+
+    }
+}
